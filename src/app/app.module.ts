ace089186ad80b13445d99df3c6dcde3461e30f7
Wesley Tejeda <wtejeda@talentpath.com>
2021-12-15 17:41:53 -0500

    Set up http service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { RankedComponent } from './ranked/ranked.component';
 import { MapsComponent } from './maps/maps.component';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { ServersComponent } from './servers/servers.component';
+import { NavComponent } from './nav/nav.component';
 
 const routes: Routes =[
   {path: "ranked", component: RankedComponent},
@@ -22,11 +23,13 @@ const routes: Routes =[
     RankedComponent,
     MapsComponent,
     ProfilesComponent,
-    ServersComponent
+    ServersComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
